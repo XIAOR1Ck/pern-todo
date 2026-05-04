2cc97ba77bb8d002edf028f2c8c0fce6c5f1459d
Suresh Acharya <sureshach.off@gmail.com>
2026-05-04 21:10:50 +0545

    Started React

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { Fragment } from 'react';
+
+// COmponents
+import InputTodo from './components/inputToDo';
+
+function App() {
+  return <Fragment>
+        <InputTodo />
+    </Fragment>
+    }
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -39,6 +39,43 @@ app.get("/todos", async(req, res) => {
     }
 });
 
+// get a todo
+
+app.get("/todos/:id", async(req, res) => {
+    try {
+       const { id } = req.params;
+        const todos = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+        res.json(todos.rows[0]);
+    } catch (err) {
+       console.error(err.message); 
+    }
+});
+
+//Update a todo
+app.put("/todos/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        console.log(description);
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+        res.json("todo was updated");
+    } catch (err) {
+       console.error(err.message); 
+    } 
+});
+
+//Delete a todo
+
+app.delete("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+        res.json("todo was deleted!!");
+    } catch (err) {
+       console.error(err.message) 
+    }
+});
+
 app.listen(5000, () => {
     console.log("The server has started!!");
 });
