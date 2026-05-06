9c78ee81ac77ff565cdb0a1ac0695a0d9495b135
Suresh Acharya <sureshach.off@gmail.com>
2026-05-06 21:42:03 +0545

    ToDo complete

--- client/src/components/listToDo.js
+++ client/src/components/listToDo.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect, useState } from "react";
+import EditTodo from "./editToDo";
 
 const ListToDos = () => {
     const [todos, setToDos] = useState([]);
@@ -55,7 +56,7 @@ const ListToDos = () => {
                         <tr key={todo.todo_id}>
                             <td>{todo.description}</td>
 
-                            <td>Edit</td>
+                            <td> <EditTodo todo ={todo} /> </td>
                             <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
                         </tr>
 
